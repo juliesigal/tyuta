948ba7188ade7c4c56582ee96d569c1f27940596
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-10-30 15:10:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def convert_words():
+# 
+#     for i in _main_ls[_choice]:
+#         print(i)
+#         for j in str(i):
+#             str1 = i.join()
+#             str1.replace('i[j]','_')
+#             _currnt_ls.append(str1)
+#     return _currnt_ls

83b00c65429942483ca76a898a1e9c279dd89ab4
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-02 19:11:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-def convert_words():
-# 
-#     for i in _main_ls[_choice]:
-#         print(i)
-#         for j in str(i):
-#             str1 = i.join()
-#             str1.replace('i[j]','_')
-#             _currnt_ls.append(str1)
-#     return _currnt_ls
+import random
+import time
+
+def convert_sent(input_lst):
+    _currnt_ls = ["_" * len(i) for i in input_lst]
+    return _currnt_ls
+
+def check_letter(a):
+    current1 = []
+    for i in lst_words:
+        str2 = ''
+        for j in i:
+            if j == _letter:
+                str2 = str2 + j
+            else:
+                str2 = str2 + '_'
+        current1.append(str2)
+    return current1
+
+
+
+
+_main_ls = [['always', 'be', 'yourself'],['beilive', 'in', 'yourself'],['never', 'say', 'never'],['dreams', 'comes', 'true'],['always', 'stay', 'strong'],['keep', 'it', 'cool'],['trust', 'in', 'yourself'],['whatever', 'you', 'like'],['the', 'rolling', 'stones'],['guns', 'and', 'roses']]
+_choice = random.randint(0,9)
+print(_choice, _main_ls[_choice])
+_currnt_ls = []
+lst_words = _main_ls[_choice]
+
+
+print(convert_sent(lst_words))
+
+_letter = input("please guess letter")
+
+print(check_letter(_letter))

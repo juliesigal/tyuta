f692789877aadcf4523e7e6dfa29b68b6c645592
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-09 16:15:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 import random
-import time
 
-def convert_sent(input_lst):
-    _currnt_ls = ["_" * len(i) for i in input_lst]
-    return _currnt_ls
+def check_letter(lst1,ltt,lst2,count):
+    for i in range(len(lst1)):
+        if lst1[i] == ltt:
+            lst2[i] = ltt
+    return "".join(lst2)
 
-def check_letter(a):
-    current1 = []
-    for i in lst_words:
-        str2 = ''
+def get_string(a):
+    lst3 = []
+    for i in a:
         for j in i:
-            if j == _letter:
-                str2 = str2 + j
-            else:
-                str2 = str2 + '_'
-        current1.append(str2)
-    return current1
-
-
+            lst3.append(j)
+        lst3.append(' ')
+    return lst3
 
 
 _main_ls = [['always', 'be', 'yourself'],['beilive', 'in', 'yourself'],['never', 'say', 'never'],['dreams', 'comes', 'true'],['always', 'stay', 'strong'],['keep', 'it', 'cool'],['trust', 'in', 'yourself'],['whatever', 'you', 'like'],['the', 'rolling', 'stones'],['guns', 'and', 'roses']]
 _choice = random.randint(0,9)
-print(_choice, _main_ls[_choice])
-_currnt_ls = []
 lst_words = _main_ls[_choice]
+counter = 0
+_currnt_ls = ["_" * len(i) for i in lst_words]
+print('Hello and welcome to the guess words game! Every right guess you get 5 points and wrong guess u lose 1 point. Good luck dear user' )
+print(''.join(_currnt_ls))
 
+new_hidden_st = get_string(lst_words)
+new_under_st = get_string(_currnt_ls)
+while '_' in new_under_st:
+    letter = input('please enter letter: ')
+    print(check_letter(new_hidden_st,letter,new_under_st,counter))
+    if letter in new_under_st:
+        counter = counter + 5
+    else:
+        counter = counter - 1
+    print('Your current score is:', counter)
 
-print(convert_sent(lst_words))
-
-_letter = input("please guess letter")
-
-print(check_letter(_letter))
+print('your final score is: ', counter)
